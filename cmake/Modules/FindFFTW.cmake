f8e06d7d2803a99b5398f92a8eed33e479f4ce9b
santi-nue <68065589+santi-nue@users.noreply.github.com>
2024-08-05 14:33:19 +0200

    Update FindFFTW.cmake

--- cmake/Modules/FindFFTW.cmake
+++ cmake/Modules/FindFFTW.cmake
@@ -26,6 +26,6 @@ find_library (FFTW3_LIBRARIES NAMES fftw3
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
+find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW3_LIBRARIES FFTW_LIBRARIES FFTW_INCLUDES)
 
-mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
+mark_as_advanced (FFTW3_LIBRARIES FFTW_LIBRARIES FFTW_INCLUDES)
