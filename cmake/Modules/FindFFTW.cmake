03adfa287456635f9df663c3c23a2afc572bff31
santi-nue <68065589+santi-nue@users.noreply.github.com>
2024-08-05 14:55:09 +0200

    Update FindFFTW.cmake

--- cmake/Modules/FindFFTW.cmake
+++ cmake/Modules/FindFFTW.cmake
@@ -22,6 +22,8 @@ find_library (FFTW_LIBRARIES NAMES fftw3f
 find_library (FFTW3_LIBRARIES NAMES fftw3
     HINTS ${PC_FFTW_LIBDIR} ${PC_FFTW_LIBRARY_DIRS})
 
+link_libraries(PkgConfig::FFTW3) 
+
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if
 # all listed variables are TRUE
